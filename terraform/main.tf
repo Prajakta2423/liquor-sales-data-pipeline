f8bdfcec9e24f1ff313ca440071cd8bd50cbd52a
Saurabh-Tripathii <98814319+Saurabh-Tripathii@users.noreply.github.com>
2026-02-04 03:15:00 +0530

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,28 @@
 provider "aws" {
-  region = var.region
+  region = "ap-south-1"
 }
 
+# -------------------------
+# S3 RAW BUCKET
+# -------------------------
 resource "aws_s3_bucket" "raw_bucket" {
-  bucket = var.raw_bucket_name
+  bucket        = var.raw_bucket_name
+  force_destroy = true
 }
 
+# -------------------------
+# S3 CLEAN BUCKET
+# -------------------------
 resource "aws_s3_bucket" "clean_bucket" {
-  bucket = var.clean_bucket_name
+  bucket        = var.clean_bucket_name
+  force_destroy = true
 }
 
+# -------------------------
+# IAM ROLE FOR GLUE
+# -------------------------
 resource "aws_iam_role" "glue_role" {
-  name = "AWSGlueServiceRole-liquor_sales"
+  name = var.glue_role_name
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -25,7 +36,10 @@ resource "aws_iam_role" "glue_role" {
   })
 }
 
+# -------------------------
+# ATTACH GLUE POLICY
+# -------------------------
 resource "aws_iam_role_policy_attachment" "glue_policy" {
   role       = aws_iam_role.glue_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSGlueServiceRole"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
 }
