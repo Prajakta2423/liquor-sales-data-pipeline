9767d6aaa94d121fb0fd6c0300eba06c518a25e3
Saurabh-Tripathii <98814319+Saurabh-Tripathii@users.noreply.github.com>
2026-02-04 03:14:24 +0530

    variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,15 @@
 variable "raw_bucket_name" {
-  description = "Raw data S3 bucket"
+  description = "Raw data S3 bucket name"
   type        = string
 }
 
 variable "clean_bucket_name" {
-  description = "Clean data S3 bucket"
+  description = "Cleaned data S3 bucket name"
   type        = string
+}
+
+variable "glue_role_name" {
+  description = "Glue IAM role name"
+  type        = string
+  default     = "AWSGlueServiceRole-liquor"
 }
